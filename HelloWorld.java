9b4a379d444ee7147cd271239188cdf6ae717832
tisean <74965226+tisean@users.noreply.github.com>
2020-11-25 11:55:15 +0300

    HelloWorld+
    Add .java HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package home.info.by;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        int c = a*b;
+        int d = a+b*c;
+        System.out.println("Hello, WORLD!!! :D "+d);
+
+    }
+}
